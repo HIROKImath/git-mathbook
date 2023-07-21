31ccbc973d36a2eb520f28d6f02d43db575a729f
EC2 Default User <ec2-user@ip-172-31-10-93.ap-northeast-1.compute.internal>
2023-07-21 06:43:01 +0000

    20230721 book controller ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,74 +1,52 @@
 class BooksController < ApplicationController
   # 参照サイト：https://qiita.com/nao0725/items/47606b8975603a12fd5e
-before_action :correct_user, only: [:edit]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
-def new
-  @book = Book.new
-end
-
-def create
-  book = Book.new(book_params)
-  book.user_id = current_user.id
-
-  if book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(book.id)
-  else
-    render :new
+  def new
+    @book = Book.new
   end
-end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
 
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+  end
 
-def index
-
-  @books = Book.all
-  @user = current_user
-
-end
+  def index
+    @books = Book.all
+    @user = current_user
+  end
 
   def show
-  #New bookの入力欄を空欄にするため、createで代入されたインスタンス変数と、別のインスタンス変数を使う
-    @book_new = Book.new
-
-
     @book = Book.find(params[:id])
     @user = @book.user
-
   end
-
-
-
 
   def edit
     @book = Book.find(params[:id])
-
-# https://qiita.com/yuuyas222/items/722945d0855466be9c89
-    # if @book.user == current_user
-    #   render "edit"
-    # else
-    #   redirect_to user_path(@book.user_id)
-    # end
-
   end
 
   def update
     @book = Book.find(params[:id])
-    # @book.update(book_params)
+
     if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
-      # @books = Book.all
       render :edit
     end
   end
 
- # booksコントローラーのindexアクションへリダイレクト
   def destroy
-    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book = Book.find(params[:id])
 
-    if @book.destroy
+    if book.destroy
       flash[:notice] ="You have destroyed book successfully."
       redirect_to books_path
     end
@@ -87,7 +65,7 @@ end
   private
   # ストロングパラメータ
     def book_params
-      params.require(:book).permit(:title, :author, :isbn, :book_image, :body)
+      params.require(:book).permit(:title, :author, :isbn, :book_image, :body, :created_at, :updated_at)
     end
 
 # https://qiita.com/nao0725/items/47606b8975603a12fd5e
